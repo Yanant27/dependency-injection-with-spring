5b4e879f145fefaaa4116c167533201b124c1687
Yanant27 <htooyanant.hyk@gmail.com>
2021-11-30 15:09:43 +0630

    Create spring DI example project

---
+++ src/main/java/hyk/springframework/DiWithSpringApplication.java
@@ -0,0 +1,13 @@
+package hyk.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiWithSpringApplication.class, args);
+    }
+
+}
