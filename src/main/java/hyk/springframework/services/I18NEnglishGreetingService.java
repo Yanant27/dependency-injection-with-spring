209fac9bf55945057ec46ab938345e056f34a0f4
Yanant27 <htooyanant.hyk@gmail.com>
2021-12-01 09:58:34 +0630

    39. Test Default Profile

--- src/main/java/hyk/springframework/services/I18NEnglishGreetingService.java
+++ src/main/java/hyk/springframework/services/I18NEnglishGreetingService.java
@@ -1,6 +1,5 @@
 package hyk.springframework.services;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
--- src/main/java/hyk/springframework/services/I18NSpanishGreetingService.java
+++ src/main/java/hyk/springframework/services/I18NSpanishGreetingService.java
@@ -1,6 +1,5 @@
 package hyk.springframework.services;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +8,7 @@ import org.springframework.stereotype.Service;
  */
 //@Service
 //@Qualifier
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18NSpanishGreetingService implements GreetingService{
     @Override
