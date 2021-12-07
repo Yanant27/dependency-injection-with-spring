833f3ecb297d0e994f34a5f4d357342f4b665c21
Yanant27 <htooyanant.hyk@gmail.com>
2021-12-07 21:56:07 +0630

    Test XML Configuration.

--- src/main/java/hyk/springframework/config/GreetingServiceConfig.java
+++ src/main/java/hyk/springframework/config/GreetingServiceConfig.java
@@ -3,14 +3,12 @@ package hyk.springframework.config;
 import hyk.springframework.repositories.EnglishGreetingRepository;
 import hyk.springframework.repositories.EnglishGreetingRepositoryImpl;
 import hyk.springframework.services.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 
 /**
  * @author Htoo Yanant Khin
  */
+@ImportResource("classpath:diwithspring-config.xml")
 @Configuration
 public class GreetingServiceConfig {
 
@@ -35,11 +33,6 @@ public class GreetingServiceConfig {
     @Bean
     PrimaryGreetingService primaryGreetingService() {
         return new PrimaryGreetingService();
-    }
-
-    @Bean
-    ConstructorGreetingService constructorGreetingService() {
-        return new ConstructorGreetingService();
     }
 
     @Bean
